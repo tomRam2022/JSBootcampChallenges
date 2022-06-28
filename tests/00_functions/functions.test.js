39ba7bba2f9ac1a1d0a7b966249979f3fe314a7f
Tomas Ramirez <you@example.com>
2022-06-28 17:48:17 -0400

    max homework

--- tests/00_functions/functions.test.js
+++ tests/00_functions/functions.test.js
@@ -5,7 +5,7 @@ const {
   max, addUnknown, evenOrOdd, operator,
 } = require('./functions');
 
-describe.only('max', () => {
+describe('max', () => {
   it('is a function', () => {
     expect(typeof max).to.equal('function');
   });
@@ -73,7 +73,7 @@ describe('evenOrOdd', () => {
   });
 });
 
-describe('operator', () => {
+describe.only('operator', () => {
   it('is a function', () => {
     expect(typeof operator).to.equal('function');
   });
